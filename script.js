f22b08844d311a96201744fd9a4891f8365fa839
eldin001 <eldin.balje@icloud.com>
2024-04-08 14:36:26 +0200

    git init

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
+
+
+const { createApp } = Vue;
+
+createApp({
+    data () {
+        return{
+            test: 'ciao',
+        }
+    },
+
+}).mount('#app')
+
